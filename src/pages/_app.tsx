640f86354e31ebbb2b213524c5298bbdf8b68c13
ojr <ojr@progit.no>
2023-04-22 13:58:47 +0200

    Clerk Fking DONE man

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,19 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import {ClerkProvider, SignedIn, SignedOut, SignInButton} from "@clerk/nextjs";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+    return (
+        <ClerkProvider {...pageProps} >
+            <SignedIn>
+                <Component {...pageProps} />
+            </SignedIn>
+            <SignedOut>
+                <SignInButton mode="modal"/>
+            </SignedOut>
+        </ClerkProvider>
+    );
+  }
 
 export default api.withTRPC(MyApp);
